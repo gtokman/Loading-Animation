93e39c8b292374cb9c633c292de6e9f9d2293a2c
Gary Tokman <hello@garytokman.me>
2019-05-28 13:34:04 -0400

    Initial Commit

---
+++ LoadingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingExample
+//
+//  Created by Gary Tokman on 5/28/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
